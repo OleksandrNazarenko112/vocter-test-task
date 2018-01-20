b8ed32d17a5b74a3b8dd8711c599b2d42a0e4f41
OleksandrNazarenko112 <nazarenkovideo@gmail.com>
2018-01-20 14:48:21 +0200

    fix for display none

--- scripts/main.js
+++ scripts/main.js
@@ -1,137 +1,128 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
+    var imagesPlansParts = [
+
+        {
+            carClass: 'mini',
+
+            plan: ['images/cars/ситроен/минимальный.jpg', 'images/cars/ситроен/стандарт.jpg',
+                'images/cars/ситроен/стандарт+.jpg', 'images/cars/ситроен/стандарт++.jpg',
+                'images/cars/ситроен/стандарт+++.jpg', 'images/cars/ситроен/целиком.jpg'
+            ],
+
+            part: ['images/cars/ситроен/оптика.jpg', 'images/cars/ситроен/пороги-внутренние.jpg',
+                'images/cars/ситроен/пороги-наружние.jpg', 'images/cars/ситроен/двери.jpg',
+                'images/cars/ситроен/задние-крылья.jpg', 'images/cars/ситроен/капот.jpg',
+                'images/cars/ситроен/передний-бампер.jpg', 'images/cars/ситроен/задний-бампер.jpg'
+            ]
+        },
+
+        {
+            carClass: 'middle',
+
+            plan: ['images/cars/mazda/минимальный.jpg', 'images/cars/mazda/стандарт.jpg',
+                'images/cars/mazda/стандарт+.jpg', 'images/cars/mazda/стандарт++.jpg',
+                'images/cars/mazda/стандарт+++.jpg', 'images/cars/mazda/целиком.jpg'
+            ],
+
+            part: ['images/cars/mazda/оптика.jpg', 'images/cars/mazda/пороги-внутренние.jpg',
+                'images/cars/mazda/пороги-наружние.jpg', 'images/cars/mazda/двери.jpg',
+                'images/cars/mazda/заднее-крыло.jpg', 'images/cars/mazda/капот.jpg',
+                'images/cars/mazda/передний-бампер.jpg', 'images/cars/mazda/задний-бампер.jpg'
+            ]
+
+        }, {
+            carClass: 'premium',
+            plan: ['images/cars/мерседес/минимальный.jpg', 'images/cars/мерседес/стандарт.jpg',
+                'images/cars/мерседес/стандарт+.jpg', 'images/cars/мерседес/стандарт++.jpg',
+                'images/cars/мерседес/стандарт+++.jpg', 'images/cars/мерседес/целиком.jpg'
+            ],
+
+            part: ['images/cars//мерседес/оптика.jpg', 'images/cars/мерседес/пороги-внутренние.jpg',
+                'images/cars/мерседес/пороги-наружние.jpg', 'images/cars/мерседес/двери.jpg',
+                'images/cars/мерседес/задние-крылья.jpg', 'images/cars/мерседес/капот.jpg',
+                'images/cars/мерседес/передний-бампер.jpg', 'images/cars/мерседес/задний-бампер.jpg'
+            ]
+        }
 
 
+    ];
 
-var imagesPlansParts = [
+    var defPlanIndex = $('.plans li .current-plan').parent().index();
 
-{
-	carClass: 'mini',
+    $(".partial-drp").click(function() {
 
-		plan: ['images/cars/ситроен/минимальный.jpg','images/cars/ситроен/стандарт.jpg',
-					'images/cars/ситроен/стандарт+.jpg','images/cars/ситроен/стандарт++.jpg',
-					'images/cars/ситроен/стандарт+++.jpg','images/cars/ситроен/целиком.jpg'],
-
-		part: ['images/cars/ситроен/оптика.jpg','images/cars/ситроен/пороги-внутренние.jpg',
-					'images/cars/ситроен/пороги-наружние.jpg','images/cars/ситроен/двери.jpg',
-					'images/cars/ситроен/задние-крылья.jpg','images/cars/ситроен/капот.jpg', 
-					'images/cars/ситроен/передний-бампер.jpg','images/cars/ситроен/задний-бампер.jpg']
-},
-
-{ 
-	carClass: 'middle',
-
-		plan: ['images/cars/mazda/минимальный.jpg','images/cars/mazda/стандарт.jpg',
-					'images/cars/mazda/стандарт+.jpg','images/cars/mazda/стандарт++.jpg',
-					'images/cars/mazda/стандарт+++.jpg','images/cars/mazda/целиком.jpg'],
-
-		part: ['images/cars/mazda/оптика.jpg','images/cars/mazda/пороги-внутренние.jpg',
-					'images/cars/mazda/пороги-наружние.jpg','images/cars/mazda/двери.jpg',
-					'images/cars/mazda/заднее-крыло.jpg','images/cars/mazda/капот.jpg', 
-					'images/cars/mazda/передний-бампер.jpg','images/cars/mazda/задний-бампер.jpg']
-
-},
-{
-	carClass: 'premium',
-	plan: ['images/cars/мерседес/минимальный.jpg','images/cars/мерседес/стандарт.jpg',
-					'images/cars/мерседес/стандарт+.jpg','images/cars/мерседес/стандарт++.jpg',
-					'images/cars/мерседес/стандарт+++.jpg','images/cars/мерседес/целиком.jpg'],
-
-	part: ['images/cars//мерседес/оптика.jpg','images/cars/мерседес/пороги-внутренние.jpg',
-					'images/cars/мерседес/пороги-наружние.jpg','images/cars/мерседес/двери.jpg',
-					'images/cars/мерседес/задние-крылья.jpg','images/cars/мерседес/капот.jpg', 
-					'images/cars/мерседес/передний-бампер.jpg','images/cars/мерседес/задний-бампер.jpg']
- }
-
-
-];
-
-	var defPlanIndex = $('.plans li .current-plan').parent().index();
-
-
-
-    $(".partial-drp").click(function(){
         $(".partial").toggle(200);
         return false;
     });
 
+    function curClassIndex() {
+        var curClassIndex = $('.car-class.current-class').index();
+        return curClassIndex;
+    }
+
+    function emptyImageWrapper() {
+        $('.plans-container .image-wrapper').empty();
+    }
+
+    function defClassImg() {
+        emptyImageWrapper()
+        $('.plans-container .image-wrapper').append('<img src=' + imagesPlansParts[curClassIndex()].plan[defPlanIndex] + '>');
+    }
+    defClassImg();
 
 
-
-function curClassIndex(){
-	 var  curClassIndex = $('.car-class.current-class').index();
-	 return curClassIndex;
-}
-function emptyImageWrapper(){
-	$('.plans-container .image-wrapper').empty();
-}
-function defClassImg(){
-			emptyImageWrapper()
- 	 	$('.plans-container .image-wrapper').append('<img src='+imagesPlansParts[curClassIndex()].plan[defPlanIndex]+'>'); 
-}
-defClassImg();
+    $('.plans li a').click(function() {
+        return false;
+    })
 
 
-$('.plans li a').click(function(){
-	return false;
-})
+    $('.plans li').on('mouseover', function(event) {
 
 
-$('.plans li').on('mouseover', function(event){
-
-	
- 	$('.plans li a').removeClass('current-plan');
- 	$(this).children().addClass('current-plan');
+        $('.plans li a').removeClass('current-plan');
+        $(this).children().addClass('current-plan');
 
 
-
-$('.modules').css('visibility', 'visible');
-
-	
- 	 	var curPlanIndex = $(this).index();
- 	 	$('.modules-container .modules').css('display', 'none');
- 	 	$('.modules-container .modules:eq(' +curPlanIndex +')').css('display', 'block');
- 	 	emptyImageWrapper()
- 	 	$('.plans-container .image-wrapper').append('<img src='+imagesPlansParts[curClassIndex()].plan[curPlanIndex]+'>'); 
+        $('.modules').css('display', 'block');
 
 
-return false;
+        var curPlanIndex = $(this).index();
+        $('.modules-container .modules').css('display', 'none');
+        $('.modules-container .modules:eq(' + curPlanIndex + ')').css('display', 'block');
+        emptyImageWrapper()
+        $('.plans-container .image-wrapper').append('<img src=' + imagesPlansParts[curClassIndex()].plan[curPlanIndex] + '>');
+
+        return false;
+    });
 
 
-});
-		
-				
-			
-
-$('.partial li').on('mouseover', function(event){
-$('.modules').css('visibility', 'hidden');
+    $('.partial li').on('mouseover', function(event) {
 
 
- 	 	var curPartIndex = $(this).index();
- 	 	emptyImageWrapper()
- 	 	$('.plans-container .image-wrapper').append('<img src='+imagesPlansParts[curClassIndex()].part[curPartIndex]+'>');
+        $('.modules').css('display', 'none');
+
+        var curPartIndex = $(this).index();
+        emptyImageWrapper()
+        $('.plans-container .image-wrapper').append('<img src=' + imagesPlansParts[curClassIndex()].part[curPartIndex] + '>');
+
+        return false;
+
+    });
 
 
+    $('.car-class a').on('click', function() {
 
-return false;
+        $(".cars-clases-container .car-class").removeClass('current-class');
+        $('.modules').css('display', 'none');
+        $(this).parent('.car-class').addClass('current-class');
 
-});
+        $('.plans li a').removeClass('current-plan');
+        $('.plans li:eq(' + defPlanIndex + ') a').addClass('current-plan');
 
-
-
-
-   
-$('.car-class a').on('click', function(){
-	 	 $(".cars-clases-container .car-class").removeClass('current-class');
-$('.modules').css('visibility', 'hidden');
-	$(this).parent('.car-class').addClass('current-class');
-	
-$('.plans li a').removeClass('current-plan');
-$('.plans li:eq(' +defPlanIndex +') a').addClass('current-plan');
-
-
-defClassImg();
- 	 	 return false;
-});
+        defClassImg();
+        return false;
+    });
 
 
 });
